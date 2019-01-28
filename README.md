9028879243ccd26bbd5067c57fd86c100c6c1711
Sodium-Hydrogen <michael.julander@utahsaint.org>
2019-01-28 16:51:21 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rune Converter #
+This is a simple python script to convert english to unicode runes.<br>
+The runes of choice for the letters j and k where added in unicode version 11 which is not widely used at this time so I'm using ᛃ and ᚲ instead.
---
+++ translate.py
@@ -0,0 +1,56 @@
+import os
+import argparse
+import string
+
+class translate:
+    def __init__(self, inputString):
+        output = self.toRunes(inputString)
+        print(output)
+
+    def toRunes(self, english):
+        csv = self.openCSV("runes.csv")
+        rune = ''
+        n = 0
+        while n < len(english):
+            char = english[n]
+            if n + 1 < len(english):
+                second = english[n + 1]
+            else:
+                second = None
+            if string.ascii_uppercase.find(char) != -1:
+                for line in csv.split('\n'):
+                    i = line.split(',')
+                    if i[0].find(char) == 0:
+                        if len(i[0]) == 2:
+                            if second is not None and i[0].rfind(second) == 1:
+                                rune += i[1]
+                                n += 1
+                                break
+                        else:
+                            rune += i[1]
+                            break
+            else:
+                rune += char
+            n += 1
+        return rune
+
+    def openCSV(self, filePath):
+        if os.path.exists(filePath):
+            file = open(filePath, 'r')
+            text = file.read()
+            file.close()
+            return text
+        else:
+            print("Unable to open %s. File not found" % filePath)
+            return None
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Convert english to runes')
+    parser.add_argument('string', type=str, help='Input string to be translated.')
+
+    args = parser.parse_args()
+    args = vars(args)
+    inputString = args['string']
+
+    tran = translate(inputString.upper())
