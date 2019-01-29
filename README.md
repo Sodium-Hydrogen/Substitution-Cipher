fbd924595e461483c3c9c3e9030f33d203f91605
Sodium-Hydrogen <michael.julander@utahsaint.org>
2019-01-29 15:32:21 -0700

    Added better functionality

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # Substitution Cipher #
 #### This is a simple python script to convert English to another format via dictionaries. ####
+```
+usage: translate.py [-h] [-l] [-r] [-u] [-D DIR] [-d DICT] [input_string]
+
+Convert english to runes
+
+positional arguments:
+  input_string          Input string to be translated.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -l, --list            List all avaliable dictionaries.
+  -r, --reverse         Convert translated string back.
+  -u, --use_case        Do not ignore capitalization of letters.
+  -D DIR, --dir DIR     Path of dictionaries directory.
+  -d DICT, --dict DICT  Dictionary to use.
+```
 I currently only have the runic dictionary made.<br>
 The runes of choice for the letters j and k where added in Unicode version 11 which is not widely used at this time so I'm using ᛃ and ᚲ instead, unlike Tolkien's runic language.<br>
 I've also created a base 10 counting system using runic symbols.<br>
--- translate.py
+++ translate.py
@@ -2,56 +2,92 @@ import os
 import argparse
 
 class translate:
-    def __init__(self, inputString):
-        output = self.toRunes(inputString.upper())
-        print(output)
+    def __init__(self, directory, file):
+        self.directory = directory.replace('/', '')
+        self.filePath = self.directory + "/" + file
 
-    def toRunes(self, english):
-        csv = self.openCSV("dictionaries/runes.csv")
-        rune = ''
-        n = 0
-        while n < len(english):
-            char = english[n]
-            if n + 1 < len(english):
-                second = english[n + 1]
-            else:
-                second = None
-            found = False
-            for line in csv.split('\n'):
-                i = line.split(',')
-                if i[0].find(char) == 0:
-                    if len(i[0]) == 2:
-                        if second is not None and i[0].rfind(second) == 1:
-                            rune += i[1]
-                            n += 1
-                            found = True
-                            break
-                    else:
-                        rune += i[1]
-                        found = True
-                        break
-            if not found:
-                rune += char
-            n += 1
-        return rune
-
-    def openCSV(self, filePath):
-        if os.path.exists(filePath):
-            file = open(filePath, 'r')
+    def openCSV(self):
+        if os.path.exists(self.filePath):
+            file = open(self.filePath, 'r')
             text = file.read()
             file.close()
             return text
         else:
-            print("Unable to open %s. File not found" % filePath)
+            print("Unable to open %s. File not found" % self.filePath)
             return None
 
+    def listCSV(self):
+        if os.path.exists(self.directory):
+            content = os.listdir(self.directory)
+            output = ''
+            for i in content:
+                if i.lower().find('.csv') != -1:
+                    self.filePath = self.directory + "/" + i
+                    output = i + " - " + self.toAlternate("ABC123") + '\n'
+            return output
+        else:
+            print("Unable to find directory: %s" % self.directory)
+            return None
+
+    def toAlternate(self, inputString):
+        return self.convert(inputString, 1, 0)
+
+    def toOriginal(self, inputString):
+        return self.convert(inputString, 0, 1)
+
+    def convert(self, inputString, toCol, fromCol):
+        csv = self.openCSV()
+        output = ''
+        n = 0
+        while n < len(inputString) and csv is not None:
+            char = inputString[n]
+            if n + 1 < len(inputString):
+                second = inputString[n + 1]
+            else:
+                second = None
+            found = False
+            for line in csv.strip().split('\n'):
+                i = line.split(',')
+                if i[fromCol].find(char) == 0:
+                    if len(i[fromCol]) == 2:
+                        if second is not None and i[fromCol].rfind(second) == 1:
+                            output += i[toCol]
+                            n += 1
+                            found = True
+                            break
+                    else:
+                        output += i[toCol]
+                        found = True
+                        break
+            if not found:
+                output += char
+            n += 1
+        if csv is None:
+            return inputString
+        else:
+            return output
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert english to runes')
-    parser.add_argument('string', type=str, help='Input string to be translated.')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('input_string', type=str, nargs='?', default=None, help='Input string to be translated.')
+    group.add_argument('-l', '--list', action='store_true', default=False, help='List all avaliable dictionaries.')
+    parser.add_argument('-r', '--reverse', action='store_true', default=False, help='Convert translated string back.')
+    parser.add_argument('-u', '--use_case', action='store_true', default=False, help='Do not ignore capitalization of letters.')
+    parser.add_argument('-D', '--dir', type=str, default='dictionaries', help='Path of dictionaries directory.')
+    parser.add_argument('-d', '--dict', type=str, default='runes.csv', help='Dictionary to use.')
 
     args = parser.parse_args()
-    args = vars(args)
-    inputString = args['string']
 
-    tran = translate(inputString.upper())
+    if not args.use_case and not args.list:
+        args.input_string = args.input_string.upper()
+
+    tran = translate(args.dir, args.dict)
+    if not args.reverse and not args.list:
+        print(tran.toAlternate(args.input_string))
+    elif not args.list:
+        print(tran.toOriginal(args.input_string))
+    else:
+        output = tran.listCSV()
+        if output is not None:
+            print("Avaliable Dictionaries:\n%s" % output, end='')
